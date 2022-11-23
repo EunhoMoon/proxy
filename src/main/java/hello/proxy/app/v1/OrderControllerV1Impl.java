4c2f32b7b7c37bf064d36ee24ba7f9ab36952eb0
Janek <eunho.moon@gmail.com>
2022-11-24 00:09:22 +0900

    Proxy Pattern V1

---
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
@@ -0,0 +1,22 @@
+package hello.proxy.app.v1;
+
+public class OrderControllerV1Impl implements OrderControllerV1 {
+
+    public final OrderServiceV1 service;
+
+    public OrderControllerV1Impl(OrderServiceV1 service) {
+        this.service = service;
+    }
+
+    @Override
+    public String request(String itemId) {
+        service.orderItem(itemId);
+        return "ok";
+    }
+
+    @Override
+    public String noLog() {
+        return "ok";
+    }
+
+}
